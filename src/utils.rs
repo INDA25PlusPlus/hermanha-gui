2abf9928a369ae8ac33bcb23ef258cc6d0aeb53e
Herman Hallonqvist <hallalay@MacBook-Air-som-tillhor-Herman.local>
2025-09-30 10:38:34 +0200

    move parsing

--- src/utils.rs
+++ src/utils.rs
@@ -45,6 +45,37 @@ pub fn to_fen(tiles: [[Tile; 8]; 8]) -> String {
     fen
 }
 
-// pub fn make_move_string(fc: usize, fr: usize, tc:usize, tr:usize) {
-//     move_string = String::new()
-// } 
+pub fn make_move_string(fc: usize, fr: usize, tc: usize, tr: usize, white:bool) -> String {
+    let files = ['A','B','C','D','E','F','G','H'];
+
+    let from_file = files[fc];
+    let from_rank = 8-fr;
+    let to_file = files[tc];
+    let to_rank = 8-tr;
+    let piece = if white { "Q" } else { "q" };
+
+    format!("{}{}{}{}{}", from_file, from_rank, to_file, to_rank, piece)
+}
+
+pub fn from_move_string(mv: String) -> (usize, usize, usize, usize, String) {
+    let files = ['A','B','C','D','E','F','G','H'];
+
+    let from_file = mv.chars().nth(0).expect("missing from file");
+    let from_rank = mv.chars().nth(1).expect("missing from rank");
+    let to_file   = mv.chars().nth(2).expect("missing to file");
+    let to_rank   = mv.chars().nth(3).expect("missing to rank");
+    let prom_piece = mv.chars().nth(4).expect("missing prom piece");
+
+    let ff_idx = files.iter()
+                      .position(|&c| c == from_file.to_ascii_uppercase())
+                      .expect("invalid from file");
+
+    let tf_idx = files.iter()
+                      .position(|&c| c == to_file.to_ascii_uppercase())
+                      .expect("invalid to file");
+
+    let fr_idx = 8-from_rank.to_digit(10).expect("invalid from rank") as usize;
+    let tr_idx = 8-to_rank.to_digit(10).expect("invalid to rank") as usize;
+
+    (ff_idx, fr_idx, tf_idx, tr_idx, prom_piece.to_string())
+}
